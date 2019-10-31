90bcdae89ec1e5c0ddead8a691299682de5f0621
Muthukumar <muthu4all@gmail.com>
2019-10-31 23:15:42 +0530

    Servlet Initial Param

---
+++ TomcatFirstDemo/src/com/muthu4all/ServletInitialParam.java
@@ -0,0 +1,51 @@
+package com.muthu4all;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletInitialParam
+ */
+@WebServlet(
+		urlPatterns = { "/ServletInitialParam" }, 
+		initParams = { 
+				@WebInitParam(name = "maxRecPerPage", value = "20", description = "Maximum number of pages")
+		})
+public class ServletInitialParam extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletInitialParam() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("Initial Param maxRecPerPage " +getInitParameter("maxRecPerPage"));
+		out.println("<br>");
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
